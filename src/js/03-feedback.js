52250f544dff2cec724f0dc43e99427f74ed020a
Roman-Pashkevych <pashkevych_roman@ukr.net>
2021-11-22 19:32:34 +0200

    task-3-feedbackForm-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+const feedbackFormData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log('відправляєм форму');
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(event) {
+    feedbackFormData[event.target.name] = event.target.value;
+    console.log(feedbackFormData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+}
+
+function populateForm() {
+    const saveFormData = localStorage.getItem(STORAGE_KEY);
+    const saveFormDataPars = JSON.parse(saveFormData);
+     console.log(saveFormDataPars);
+    
+    if (saveFormDataPars !== null) {
+    form["email"].value = saveFormDataPars.email;
+    form["message"].value = saveFormDataPars.message;
+};
+}
